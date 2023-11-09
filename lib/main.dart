6a3c13f28d940f2d867516043b5b777d07b75ddf
Maksym Solomko <max.exc7@gmail.com>
2023-11-09 14:28:36 +0100

    t_6_add_logger
    add logger

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:weather_app_dart/scr/core/di/main_weather_injection_container.dart'
     as main_weather_injection;
+import 'package:weather_app_dart/scr/core/di/external_enjection_container.dart'
+    as external_enjection;
 import 'package:weather_app_dart/scr/features/main_page/main_weater_page.dart';
 
 void main() {
   main_weather_injection.init();
+  external_enjection.init();
   runApp(const Main());
 }
 
