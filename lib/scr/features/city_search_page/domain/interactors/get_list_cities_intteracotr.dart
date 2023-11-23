aba16ee3c0ba4768fde6981ab68e18addd4f5ced
stanislav spasonov <s.spasonov@itransition.com>
2023-11-23 15:37:49 +0100

    add getListCitiesInteractor

---
+++ lib/scr/features/city_search_page/domain/interactors/get_list_cities_intteracotr.dart
@@ -0,0 +1,13 @@
+import 'package:weather_app_dart/scr/core/domain/base_interactor.dart';
+import 'package:weather_app_dart/scr/features/city_search_page/domain/cities_repository.dart';
+import 'package:weather_app_dart/scr/features/city_search_page/domain/city_entity.dart';
+
+class GetListCitiesInteractor extends BaseInteractor<String, List<CityEntity>> {
+  final CityRepository cityRepository;
+
+  GetListCitiesInteractor(this.cityRepository);
+  @override
+  Future<List<CityEntity>> call(String argument) async {
+    return await cityRepository.getListCities(argument);
+  }
+}
